d83224b999262022f3c3493a01741481fd2640d0
Parag More <paragmore306@gmail.com>
2020-10-31 11:35:44 +0530

    Initial Commit with basic mern app setup.

---
+++ back/index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use('/posts', postRoutes)
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const MONGO_URL = 'mongodb+srv://parag:7iw5r9CBFfXHZN3@cluster0.qcwgq.mongodb.net/<dbname>?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=>{
+        app.listen(PORT, ()=>{
+            console.log(`Server running on: ${PORT}.\nSuccessfully connected to MONGO DB` )
+        })   
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+
+    mongoose.set('useFindAndModify', false);
