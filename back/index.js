ef79699c01ecd7f21943a0798778dfdf85819490
Parag More <paragmore306@gmail.com>
2020-11-01 11:11:58 +0530

    Added actions and reducers for posting the form data.

--- back/index.js
+++ back/index.js
@@ -7,11 +7,11 @@ import postRoutes from './routes/posts.js'
 
 const app = express();
 
-app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
+app.use('/posts', postRoutes)
 
 const MONGO_URL = 'mongodb+srv://parag:7iw5r9CBFfXHZN3@cluster0.qcwgq.mongodb.net/<dbname>?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
